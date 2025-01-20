e0bee2797df949006c48cbbbb9706b0dfd667950
Fuli Wang <52630571+Fuli-Wang@users.noreply.github.com>
2025-01-20 15:29:12 +0000

    Add files via upload
    The IP's configuration file

---
+++ remote-dingoA.sh
@@ -0,0 +1,2 @@
+export ROS_MASTER_URI=http://cpr-do100-10000059:11311  #Dingo's IP. you may also use export ROS_MASTER_URI=http://192.168.1.2:11311 (need to confirm the IP)
+export ROS_IP=192.168.1.5 #PC/laptop(offboard computer) IP 
